e84fd2efd2c6a156278b52376562e6deb462aa0b
lucianadematteis <lucianadematteis1999@gmail.com>
2023-07-31 13:34:17 -0300

    Update login.java

--- src/gui/login.java
+++ src/gui/login.java
@@ -23,7 +23,7 @@ public class login extends JFrame {
 	private JTextField user;
 	private JTextField pass;
 
-	public static void main(String[] args) {
+	public static void main(String[] args) { //BORRAR LUEGO
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
