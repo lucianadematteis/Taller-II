b1e6d405b8493f79aa003aaf501a5b5de67db0dc
DailiUmineko <daiana_daguerre@hotmail.com>
2023-08-03 18:41:41 -0300

    Update registrarUsuario.java

--- src/gui/registrarUsuario.java
+++ src/gui/registrarUsuario.java
@@ -8,10 +8,17 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.border.LineBorder;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JSeparator;
 
 public class registrarUsuario extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField usuario;
+	private JTextField pass;
+	private JTextField pass2;
 
 	/**
 	 * Launch the application.
@@ -54,9 +61,55 @@ public class registrarUsuario extends JFrame {
 		JPanel panel = new JPanel();
 		panel.setBorder(new LineBorder(new Color(0, 0, 0), 1, true));
 		panel.setBackground(recuadro);
-		panel.setBounds(250, 228, 471, 323);
+		panel.setBounds(264, 173, 471, 344);
 		contentPane.add(panel);
 		panel.setLayout(null);
+		
+		usuario = new JTextField();
+		usuario.setBounds(211, 77, 198, 20);
+		panel.add(usuario);
+		usuario.setColumns(10);
+		
+		pass = new JTextField();
+		pass.setBounds(211, 140, 198, 20);
+		panel.add(pass);
+		pass.setColumns(10);
+		
+		pass2 = new JTextField();
+		pass2.setBounds(211, 188, 198, 20);
+		panel.add(pass2);
+		pass2.setColumns(10);
+		
+		JLabel lblNewLabel = new JLabel("Ingrese un usuario");
+		lblNewLabel.setForeground(Color.WHITE);
+		lblNewLabel.setFont(new Font("SansSerif", Font.PLAIN, 16));
+		lblNewLabel.setBounds(63, 74, 157, 20);
+		panel.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("Nueva contrase\u00F1a");
+		lblNewLabel_1.setForeground(Color.WHITE);
+		lblNewLabel_1.setFont(new Font("SansSerif", Font.PLAIN, 16));
+		lblNewLabel_1.setBounds(63, 139, 172, 17);
+		panel.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("Confirmar contrase\u00F1a");
+		lblNewLabel_2.setForeground(Color.WHITE);
+		lblNewLabel_2.setFont(new Font("SansSerif", Font.PLAIN, 16));
+		lblNewLabel_2.setBounds(37, 187, 172, 17);
+		panel.add(lblNewLabel_2);
+		
+		JSeparator separator = new JSeparator();
+		separator.setBounds(21, 115, 440, 2);
+		panel.add(separator);
+		
+		JLabel lblNewLabel_3 = new JLabel("REGISTRO DE USUARIO");
+		lblNewLabel_3.setForeground(Color.WHITE);
+		lblNewLabel_3.setFont(new Font("SansSerif", Font.PLAIN, 36));
+		lblNewLabel_3.setBounds(276, 11, 545, 99);
+		contentPane.add(lblNewLabel_3);
+		
+		JSeparator separator_1 = new JSeparator();
+		separator_1.setBounds(10, 96, 988, 2);
+		contentPane.add(separator_1);
 	}
-
 }
