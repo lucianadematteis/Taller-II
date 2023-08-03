e3761c247fcbd65398f31e48a9824b366bea531c
DailiUmineko <daiana_daguerre@hotmail.com>
2023-08-03 18:27:16 -0300

    Update registrarUsuario.java

--- src/gui/registrarUsuario.java
+++ src/gui/registrarUsuario.java
@@ -32,7 +32,7 @@ public class registrarUsuario extends JFrame {
 	 */
 	public registrarUsuario() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 1024,700);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
