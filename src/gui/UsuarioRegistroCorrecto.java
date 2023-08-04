2d29c0257fbbdab3a6258827ed150ec01d968a29
DailiUmineko <daiana_daguerre@hotmail.com>
2023-08-04 10:02:58 -0300

    Update UsuarioRegistroCorrecto.java

--- src/gui/UsuarioRegistroCorrecto.java
+++ src/gui/UsuarioRegistroCorrecto.java
@@ -47,6 +47,7 @@ public class UsuarioRegistroCorrecto extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
+		contentPane.setBackground(fondoPrincipal);
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
