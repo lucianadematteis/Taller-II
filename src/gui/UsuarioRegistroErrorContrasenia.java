14bc9c4349cbddcc4a33d78c7f09991744ba7498
DailiUmineko <daiana_daguerre@hotmail.com>
2023-08-04 10:24:12 -0300

    Create UsuarioRegistroErrorContrasenia.java

---
+++ src/gui/UsuarioRegistroErrorContrasenia.java
@@ -0,0 +1,66 @@
+package gui;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class UsuarioRegistroErrorContrasenia extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					UsuarioRegistroErrorContrasenia frame = new UsuarioRegistroErrorContrasenia();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public UsuarioRegistroErrorContrasenia() {
+		
+		Color fondoPrincipal = new Color (66,141,138);
+		Color fondoVentana = new Color (187,218,219);
+		Color fuentePrincipal = new Color (255,255,255);
+		Color escritura = new Color (0,0,0);
+		Color botones = new Color (3,60,88);
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBackground(fondoPrincipal);
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JButton aceptar = new JButton("ACEPTAR");
+		aceptar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				dispose();
+			}
+		});
+		aceptar.setForeground(fuentePrincipal);
+		aceptar.setBackground(botones);
+		aceptar.setBounds(173, 215, 89, 23);
+		aceptar.setFocusPainted(false);
+		contentPane.add(aceptar);
+	}
+
+}
