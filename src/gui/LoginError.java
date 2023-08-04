9c7cfe0e13b23023cf36e7225ddb77bad7e098fb
DailiUmineko <daiana_daguerre@hotmail.com>
2023-08-04 12:47:19 -0300

    Create LoginError.java

---
+++ src/gui/LoginError.java
@@ -0,0 +1,51 @@
+package gui;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.Color;
+
+public class LoginError extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					LoginError frame = new LoginError();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public LoginError() {
+		
+		Color fondoPrincipal = new Color (66,141,138);
+		Color fondoVentana = new Color (187,218,219);
+		Color fuentePrincipal = new Color (255,255,255);
+		Color escritura = new Color (0,0,0);
+		Color botones = new Color (3,60,88);
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBackground(fondoPrincipal);
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+	}
+
+}
