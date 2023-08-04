17f7e57b8fd2aa59f2a62f68526db4eb0e498f2c
DailiUmineko <daiana_daguerre@hotmail.com>
2023-08-04 09:50:54 -0300

    Update UsuarioRegistroCorrecto.java

--- src/gui/UsuarioRegistroCorrecto.java
+++ src/gui/UsuarioRegistroCorrecto.java
@@ -6,6 +6,9 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import java.awt.Font;
 
 public class UsuarioRegistroCorrecto extends JFrame {
 
@@ -35,8 +38,16 @@ public class UsuarioRegistroCorrecto extends JFrame {
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JButton aceptar = new JButton("ACEPTAR");
+		aceptar.setBounds(173, 215, 89, 23);
+		contentPane.add(aceptar);
+		
+		JLabel lblNewLabel = new JLabel("El usuario se ha registrado correctamente");
+		lblNewLabel.setFont(new Font("SansSerif", Font.PLAIN, 20));
+		lblNewLabel.setBounds(32, 27, 485, 155);
+		contentPane.add(lblNewLabel);
 	}
-
 }
