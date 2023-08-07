a6be2f9fcf0a67514e92dfda998fa382a8a6136f
DailiUmineko <daiana_daguerre@hotmail.com>
2023-08-07 19:50:15 -0300

    Update BMUsuario.java

--- src/gui/BMUsuario.java
+++ src/gui/BMUsuario.java
@@ -14,10 +14,13 @@ import javax.swing.JPanel;
 import javax.swing.JSeparator;
 import javax.swing.border.EmptyBorder;
 import javax.swing.border.LineBorder;
+import javax.swing.JTextField;
 
 public class BMUsuario extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField pass;
+	private JTextField pass2;
 
 	/**
 	 * Launch the application.
@@ -56,23 +59,23 @@ public class BMUsuario extends JFrame {
 		JPanel panel = new JPanel();
 		panel.setBorder(new LineBorder(new Color(0, 0, 0), 1, true));
 		panel.setBackground(recuadro);
-		panel.setBounds(22, 135, 471, 484);
+		panel.setBounds(47, 156, 456, 353);
 		contentPane.add(panel);
 		panel.setLayout(null);
 		
-		JLabel lblNewLabel_1 = new JLabel("Nueva contrase\u00F1a");
+		JLabel lblNewLabel_1 = new JLabel("Ingrese nueva contrase\u00F1a");
 		lblNewLabel_1.setForeground(fuentePrincipal);
 		lblNewLabel_1.setFont(new Font("SansSerif", Font.PLAIN, 20));
-		lblNewLabel_1.setBounds(128, 143, 225, 17);
+		lblNewLabel_1.setBounds(92, 102, 250, 39);
 		panel.add(lblNewLabel_1);
 		
-		JLabel lblNewLabel_2 = new JLabel("Confirmar contrase\u00F1a");
+		JLabel lblNewLabel_2 = new JLabel("Repita la contrase\u00F1a");
 		lblNewLabel_2.setForeground(fuentePrincipal);
 		lblNewLabel_2.setFont(new Font("SansSerif", Font.PLAIN, 20));
-		lblNewLabel_2.setBounds(98, 208, 199, 17);
+		lblNewLabel_2.setBounds(92, 202, 205, 26);
 		panel.add(lblNewLabel_2);
 		
-		JButton aceptar = new JButton("ACEPTAR");
+		JButton aceptar = new JButton("CONFIRMAR");
 		aceptar.setForeground(fuentePrincipal);
 		aceptar.setBackground(botones);
 		aceptar.addActionListener(new ActionListener() {
@@ -94,28 +97,33 @@ public class BMUsuario extends JFrame {
 			}
 		});
 		aceptar.setFont(new Font("SansSerif", Font.PLAIN, 11));
-		aceptar.setBounds(341, 285, 104, 23);
+		aceptar.setBounds(316, 302, 104, 23);
 		aceptar.setFocusPainted(false);
 		panel.add(aceptar);
 		
-		JButton btnCancelar = new JButton("CANCELAR");
-		btnCancelar.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				login frame = new login();
-				frame.setVisible(true);
-				dispose();
-			}
-		});
-		btnCancelar.setForeground(Color.WHITE);
-		btnCancelar.setFont(new Font("SansSerif", Font.PLAIN, 11));
-		btnCancelar.setFocusPainted(false);
-		btnCancelar.setBackground(new Color(3, 60, 88));
-		btnCancelar.setBounds(213, 285, 104, 23);
-		panel.add(btnCancelar);
+		JLabel lblNewLabel = new JLabel("CAMBIAR CONTRASE\u00D1A");
+		lblNewLabel.setFont(new Font("SansSerif", Font.BOLD, 24));
+		lblNewLabel.setForeground(Color.WHITE);
+		lblNewLabel.setBounds(70, 11, 328, 55);
+		panel.add(lblNewLabel);
+		
+		JSeparator separator = new JSeparator();
+		separator.setBounds(22, 77, 392, 2);
+		panel.add(separator);
+		
+		pass = new JTextField();
+		pass.setBounds(92, 152, 235, 20);
+		panel.add(pass);
+		pass.setColumns(10);
+		
+		pass2 = new JTextField();
+		pass2.setBounds(92, 234, 229, 20);
+		panel.add(pass2);
+		pass2.setColumns(10);
 		
 		JLabel lblNewLabel_3 = new JLabel("MODIFICACI\u00D3N DE USUARIO");
 		lblNewLabel_3.setForeground(Color.WHITE);
-		lblNewLabel_3.setFont(new Font("SansSerif", Font.PLAIN, 36));
+		lblNewLabel_3.setFont(new Font("SansSerif", Font.BOLD, 36));
 		lblNewLabel_3.setBounds(276, 11, 545, 99);
 		contentPane.add(lblNewLabel_3);
 		
@@ -125,5 +133,4 @@ public class BMUsuario extends JFrame {
 		
 		
 	}
-
 }
