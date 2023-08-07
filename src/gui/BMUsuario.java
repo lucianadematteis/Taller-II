c51cea14adbea7d75099b7548a7dac53f375bb81
DailiUmineko <daiana_daguerre@hotmail.com>
2023-08-07 19:29:26 -0300

    Update BMUsuario.java

--- src/gui/BMUsuario.java
+++ src/gui/BMUsuario.java
@@ -1,6 +1,7 @@
 package gui;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
@@ -31,12 +32,18 @@ public class BMUsuario extends JFrame {
 	 * Create the frame.
 	 */
 	public BMUsuario() {
+		Color fondoPrincipal = new Color (66,141,138);
+		Color fondoVentana = new Color (187,218,219);
+		Color fuentePrincipal = new Color (255,255,255);
+		Color escritura = new Color (0,0,0);
+		Color botones = new Color (3,60,88);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 1024,700);
 		contentPane = new JPanel();
+		contentPane.setBackground(fondoPrincipal);
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
 	}
 
 }
