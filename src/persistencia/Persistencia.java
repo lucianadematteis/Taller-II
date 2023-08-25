69f0ca2d1b6c665c6e62aac31f7722f74d7dd263
lucianadematteis <lucianadematteis1999@gmail.com>
2023-08-25 16:20:37 -0300

    Metodo persistir registros

--- src/persistencia/Persistencia.java
+++ src/persistencia/Persistencia.java
@@ -8,6 +8,10 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
 
+import logica.Atributo;
+import logica.Cadena;
+import logica.Entero;
+
 public class Persistencia {
 
 	public int identificarSistema() {
@@ -27,6 +31,71 @@ public class Persistencia {
         	return -1;
         }
 		
+	}
+	
+	public void persistirRegistros(ArrayList<Map<String, Atributo>> registros, String nombreUsuario, String nombreTabla, String nombreBD){
+		
+		String nombreArchivo="";
+		
+		if (identificarSistema()==1) { //Si es windows
+			
+			nombreArchivo = System.getProperty("user.home") + "\\Desktop\\Sistema\\" + nombreUsuario + "\\" + nombreTabla + "\\" + nombreBD + "\\" + "Registros.txt";
+			
+		}else if(identificarSistema()==0){ //Si es linux
+			
+			nombreArchivo = System.getProperty("user.home") + "//Desktop//Sistema//" + nombreUsuario + "//" + nombreTabla + "//" + nombreBD + "//" + "Registros.txt";
+			
+		}
+		
+		try {
+			
+		    FileWriter archivo = new FileWriter(nombreArchivo, true);
+
+		    for (Map<String, Atributo> registro : registros) {
+		    	
+		        String registroFinal = ""; // Reiniciar para cada nuevo registro
+		        int atributoActual = 0;
+
+		        for (Map.Entry<String, Atributo> posicion : registro.entrySet()) {
+		        	
+		            Atributo atributo = posicion.getValue();
+		            atributoActual++;
+
+		            if (atributo instanceof Cadena) {
+		            	
+		                Cadena cadena = (Cadena) atributo;
+		                String dato = cadena.getDato();
+		                registroFinal += dato;
+		                
+		            } else if (atributo instanceof Entero) {
+		            	
+		                Entero entero = (Entero) atributo;
+		                int valor = entero.getValor();
+		                registroFinal += Integer.toString(valor);
+		                
+		            }
+
+		            if (atributoActual < registro.size()) {
+		            	
+		                registroFinal += ":";
+		                
+		            }
+		            
+		        }
+
+		        registroFinal += "|"; // Agregar el carácter "|" al final de cada registro
+
+		        archivo.write(registroFinal + "\n");
+		    }
+
+		    archivo.close();
+		    
+		} catch (IOException e) {
+		    
+			System.out.println("Ocurrió un error al persistir los datos en el archivo: " + e.getMessage());
+		
+		}
+
 	}
 	
 }
