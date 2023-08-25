7eb1cf129cc3a1a931587949632a620ce1aac89d
lucianadematteis <lucianadematteis1999@gmail.com>
2023-08-25 12:42:39 -0300

    Update Persistencia.java

--- src/persistencia/Persistencia.java
+++ src/persistencia/Persistencia.java
@@ -1,5 +1,32 @@
 package persistencia;
 
+import java.util.StringTokenizer;//tokenizador
+import java.io.FileWriter;//escritura
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
 public class Persistencia {
 
+	public int identificarSistema() {
+		
+		String so = System.getProperty("os.name").toLowerCase();
+        
+        if (so.contains("win")) {
+        	
+            return 1;
+            
+        } else if (so.contains("nix") || so.contains("nux") || so.contains("mac")) {
+        	
+            return 0;
+            
+        } else {
+        	
+        	return -1;
+        }
+		
+	}
+	
 }
