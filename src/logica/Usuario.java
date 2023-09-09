000231f96965957f10b82b2c67134cf9a10dcb27
DailiUmineko <daiana_daguerre@hotmail.com>
2023-09-08 23:25:10 -0300

    Update Usuario.java

--- src/logica/Usuario.java
+++ src/logica/Usuario.java
@@ -23,6 +23,7 @@ public class Usuario {
 		this.nombreUser = nombreUser;
 		this.contrasenia = contrasenia;
 		basesDatos = new LinkedHashMap<String, BaseDatos>(); //Si se pone en estado de null, se rompe el programa y no se puede persistir
+		//abajo queda comentado el cómo estaba antes
 		//this.basesDatos = null; // Establece el mapa de bases de datos como null
 	}
 
