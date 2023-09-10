b7b45b36cc55ccd05bff7caa66190fa30002d60d
mdgonzalezok <mauriciodaniel13@gmail.com>
2023-09-10 09:45:19 -0300

    Update FachadaLogica.java
    Se incorporan metodos obtenerCantidad, obtenerMin, obtenerMax

--- src/comunicacion/FachadaLogica.java
+++ src/comunicacion/FachadaLogica.java
@@ -14,121 +14,172 @@ public class FachadaLogica implements IFachadaLogica {
 	private String tabla;
 	private LinkedHashMap<String, Usuario> usuarios;
 	private LinkedHashMap<String, String> ayuda;
-	
+
 	public FachadaLogica() {
-		
+
 		baseDatos = "";
 		usuario = "";
 		tabla = "";
 		usuarios = new LinkedHashMap<String, Usuario>();
 		ayuda = new LinkedHashMap<String, String>();
-		
+
 	}
 
 	public String getBaseDatos() {
-		
+
 		return baseDatos;
-		
+
 	}
 
 	public void setBaseDatos(String baseDatos) {
-		
+
 		this.baseDatos = baseDatos;
-		
+
 	}
 
 	public String getUsuario() {
-		
+
 		return usuario;
-		
+
 	}
 
 	public void setUsuario(String usuario) {
-		
+
 		this.usuario = usuario;
-		
+
 	}
 
 	public String getTabla() {
-		
+
 		return tabla;
-		
+
 	}
 
 	public void setTabla(String tabla) {
-		
+
 		this.tabla = tabla;
-		
+
 	}
 
 	public LinkedHashMap<String, Usuario> getUsuarios() {
-		
+
 		return usuarios;
-		
+
 	}
 
 	public void setUsuarios(LinkedHashMap<String, Usuario> usuarios) {
-		
+
 		this.usuarios = usuarios;
-		
+
 	}
 
 	public Map<String, String> getAyuda() {
-		
+
 		return ayuda;
-		
+
 	}
 
 	public void setAyuda(LinkedHashMap<String, String> ayuda) {
-		
+
 		this.ayuda = ayuda;
-		
+
 	}
-	
+
 	public void persistirDatos(LinkedHashMap<String, Usuario> usuarios) {
-		
+
 		persistencia.persistirTodo(usuarios);
-		
+
 	}
-	
+
 	public void recuperarDatos(LinkedHashMap<String, Usuario> usuarios) {
-		
+
 		persistencia.recuperarTodo(usuarios);
-		
+
 	}
-	//Dai mod usuario a partir de un dto usuario
-	public void modificarUsuario(DTOUsuario user){
-		Usuario usAux = new Usuario (user);
+
+	// Dai mod usuario a partir de un dto usuario
+	public void modificarUsuario(DTOUsuario user) {
+		Usuario usAux = new Usuario(user);
 		usuarios.replace(usAux.getNombreUser(), usAux);
 	}
-	
+
 	public void eliminarusuario(DTOUsuario user) {
 		Usuario usu = new Usuario(user);
 		usuarios.remove(usu);
-		}
-	
-	public Usuario obtenerUsuario(String nombreUse, LinkedHashMap<String, Usuario>[] usuarios1) {
-	 
-
-	    for (LinkedHashMap<String, Usuario> usuarioMap : usuarios1) {
-	     
-	        Usuario usuario1 = usuarioMap.get(nombreUse);
-	        if (usuario1 != null) {
-	            return usuario1; // o
-	        }
-	    }
-	    return null; // 
 	}
-	
-	//recibe un arraylist con los valores y devuelve el promedio
+
+	public Usuario obtenerUsuario(String nombreUse, LinkedHashMap<String, Usuario>[] usuarios1) {
+
+		for (LinkedHashMap<String, Usuario> usuarioMap : usuarios1) {
+
+			Usuario usuario1 = usuarioMap.get(nombreUse);
+			if (usuario1 != null) {
+				return usuario1; // o
+			}
+		}
+		return null; //
+	}
+
+	public int obtenerCantidad(ArrayList<Integer> valores) {
+
+		return valores.size();
+	}
+
+	public int obtenerMaximo(ArrayList<Integer> valores) {
+
+		if (valores.isEmpty()) {
+
+			throw new IllegalArgumentException("El ArrayList está vacío, no se puede obtener el máximo");
+
+		}
+
+		int maximo = valores.get(0);
+
+		for (int i = 1; i < valores.size(); i++) {
+
+			if (valores.get(i) > maximo) {
+
+				maximo = valores.get(i);
+
+			}
+
+		}
+
+		return maximo;
+
+	}
+
+	public int obtenerMinimo(ArrayList<Integer> valores) {
+
+		if (valores.isEmpty()) {
+
+			throw new IllegalArgumentException("El ArrayList está vacío, no se puede obtener el mínimo");
+
+		}
+
+		int minimo = valores.get(0);
+
+		for (int i = 1; i < valores.size(); i++) {
+
+			if (valores.get(i) < minimo) {
+
+				minimo = valores.get(i);
+
+			}
+		}
+
+		return minimo;
+
+	}
+
+	// recibe un arraylist con los valores y devuelve el promedio
 	public int obtenerPromedio(ArrayList<Integer> valores) {
-		 int suma = 0;
-		    for (int i = 0; i < valores.size(); i++) {
-		        suma += valores.get(i);
-		    }
-		    int promedio = suma / valores.size();
+		int suma = 0;
+		for (int i = 0; i < valores.size(); i++) {
+			suma += valores.get(i);
+		}
+		int promedio = suma / valores.size();
 		return promedio;
 	}
-	
-	
+
 }
