da4b5e6c36409f4fc1904c0bdcac38286c8f94ef
DailiUmineko <daiana_daguerre@hotmail.com>
2023-09-17 22:14:35 -0300

    Create UsuarioCambioContraseniaCorrecto.java

---
+++ src/gui/UsuarioCambioContraseniaCorrecto.java
@@ -0,0 +1,55 @@
+package gui;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class UsuarioCambioContraseniaCorrecto extends JFrame {
+
+	private JPanel contentPane;
+
+	public UsuarioCambioContraseniaCorrecto() {
+		Color fondoPrincipal = new Color (66,141,138);
+		Color fuentePrincipal = new Color (255,255,255);
+		Color escritura = new Color (0,0,0);
+		Color botones = new Color (3,60,88);
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setBackground(fondoPrincipal);
+		
+		JButton aceptar = new JButton("ACEPTAR");
+		aceptar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				dispose();
+			}
+		});
+		contentPane.setLayout(null);
+		
+		aceptar.setForeground(fuentePrincipal);
+		aceptar.setBackground(botones);
+		aceptar.setBounds(173, 215, 89, 23);
+		aceptar.setFocusPainted(false);
+		contentPane.add(aceptar);
+		
+		JLabel lblNewLabel = new JLabel("La contrase\u00F1a se ha cambiado con \u00E9xito");
+		lblNewLabel.setForeground(escritura);
+		lblNewLabel.setFont(new Font("SansSerif", Font.PLAIN, 20));
+		lblNewLabel.setBounds(38, 49, 485, 155);
+		contentPane.add(lblNewLabel);
+	}
+
+}
