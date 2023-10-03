3b4a98058b50889429311807bf41ef062d581b25
DailiUmineko <daiana_daguerre@hotmail.com>
2023-10-03 17:14:02 -0300

    Update UsuarioRegistroErrorNombre.java

--- src/gui/UsuarioRegistroErrorNombre.java
+++ src/gui/UsuarioRegistroErrorNombre.java
@@ -9,6 +9,7 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.SwingConstants;
 
 public class UsuarioRegistroErrorNombre extends JFrame {
 
@@ -30,7 +31,7 @@ public class UsuarioRegistroErrorNombre extends JFrame {
 		contentPane.setLayout(null);
 		
 		JButton aceptar = new JButton("ACEPTAR");
-		aceptar.setFont(new Font("Verdana", Font.BOLD, 14));
+		aceptar.setFont(new Font("Verdana", Font.BOLD, 16));
 		aceptar.addActionListener(new ActionListener() {
 			
 			public void actionPerformed(ActionEvent e) {
@@ -42,15 +43,16 @@ public class UsuarioRegistroErrorNombre extends JFrame {
 		});
 		aceptar.setForeground(fuentePrincipal);
 		aceptar.setBackground(botones);
-		aceptar.setBounds(166, 202, 102, 36);
+		aceptar.setBounds(154, 191, 135, 48);
 		aceptar.setFocusPainted(false);
 		contentPane.add(aceptar);
 		
 		
-		JLabel lblNewLabel = new JLabel("Nombre de usuario incorrecto");
+		JLabel lblNewLabel = new JLabel("<HTML> <CENTER> ERROR <BR> <BR> EL USUARIO YA EXISTE <BR> EN EL SISTEMA </CENTER> </HTML>");
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNewLabel.setForeground(Color.YELLOW);
-		lblNewLabel.setFont(new Font("Verdana", Font.PLAIN, 20));
-		lblNewLabel.setBounds(81, 36, 314, 155);
+		lblNewLabel.setFont(new Font("Verdana", Font.BOLD, 20));
+		lblNewLabel.setBounds(10, 36, 414, 155);
 		contentPane.add(lblNewLabel);
 		
 		JLabel lblNewLabel_1 = new JLabel("C\u00F3digo de error 6");
