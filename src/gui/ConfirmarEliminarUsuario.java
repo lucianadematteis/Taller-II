79b9b3169e4645998811ae0eb778b807f6fc4784
DailiUmineko <daiana_daguerre@hotmail.com>
2023-10-05 21:06:27 -0300

    Update ConfirmarEliminarUsuario.java

--- src/gui/ConfirmarEliminarUsuario.java
+++ src/gui/ConfirmarEliminarUsuario.java
@@ -28,7 +28,7 @@ public class ConfirmarEliminarUsuario extends JFrame {
 		Color fuentePrincipal = new Color (255,255,255);
 		Color botones = new Color (3,60,88);
 
-		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBackground(fondoPrincipal);
